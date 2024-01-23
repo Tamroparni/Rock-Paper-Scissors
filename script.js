62e1005232866a52eeb6dc7a9481a50c972ebf9d
Tamroparni <149408750+Tamroparni@users.noreply.github.com>
2024-01-23 13:29:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let score = JSON.parse(localStorage.getItem('score')) || {
 wins: 0,
 losses: 0,
@@ -6,6 +5,47 @@ ties: 0
 };
 
 updateScoreElement();
+
+let isAutoPlaying = false;
+let intervalId;
+
+function autoPlay(){
+    if(!isAutoPlaying) {
+        intervalId = setInterval(() => {
+            const playerMove = pickComputerMove();
+            playGame(playerMove);
+        }, 1000);
+        isAutoPlaying = true;
+    } else {
+        clearInterval(intervalId);
+        isAutoPlaying = false;
+    }
+}
+
+document.querySelector('.js-rock-button')
+    .addEventListener('click', () => {
+        playGame('rock');
+    });
+
+document.querySelector('.js-paper-button')
+    .addEventListener('click', () => {
+        playGame('paper');
+    });
+
+document.querySelector('.js-scissors-button')
+    .addEventListener('click', () => {
+        playGame('scissors');
+    });    
+
+document.body.addEventListener('keydown', (event) =>{
+    if(event.key === 'r'){
+        playGame('rock');
+    } else if (event.key === 'p'){
+        playGame('paper');
+    } else if (event.key === 's'){
+        playGame('scissors');
+    }
+});
 
 function playGame(playerMove) {
     const computerMove = pickComputerMove();
